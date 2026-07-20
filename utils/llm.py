093579534b0880bd5aed94e48f7852c649126a4d
Harsh Bhatanagar <harsh210806@gmail.com>
2026-07-20 17:44:37 +0530

    chnaged llama_scout

--- utils/llm.py
+++ utils/llm.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 groq_api_key=os.getenv("GROQ_API_KEY")
+openrouter_api_key=os.getenv("OPENROUTER_API_KEY")
 
 groq_url="https://api.groq.com/openai/v1"
-
+openrouter_url="https://openrouter.ai/api/v1"
 groq=OpenAI(base_url=groq_url,api_key=groq_api_key)
+openrouter=OpenAI(base_url=openrouter_url,api_key=openrouter_api_key)
 
 
 def llm(user_prompt,system_prompt,response_format=None):
@@ -16,8 +18,8 @@ def llm(user_prompt,system_prompt,response_format=None):
         response=groq.chat.completions.create(model="llama-3.3-70b-versatile",messages=messages)
         return response.choices[0].message.content
     else:
-        response = groq.beta.chat.completions.parse(
-        model="meta-llama/llama-4-scout-17b-16e-instruct",
+        response = openrouter.beta.chat.completions.parse(
+        model="google/gemma-4-26b-a4b-it:free",
         messages=messages,
         response_format=response_format,
         )
